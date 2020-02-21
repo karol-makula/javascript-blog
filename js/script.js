be11dc9ed1f3b9184f98b97d10e80fcff8cf7c48
Karol Makuła <karol.makula@gmail.com>
2020-02-21 18:29:28 +0100

    Add spaces before parentheses & Remove console.log

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 {
     'use strict';
 
-    const titleClickHandler = function(){
+    const titleClickHandler = function() {
 
         event.preventDefault();
 
@@ -11,7 +11,7 @@
 
         const activeLinks = document.querySelectorAll('.titles a.active');
 
-        for(let activeLink of activeLinks){
+        for (let activeLink of activeLinks) {
             activeLink.classList.remove('active');
         }
 
@@ -23,7 +23,7 @@
 
         const activeArticles = document.querySelectorAll('.post.active');
 
-        for(let activeArticle of activeArticles){
+        for (let activeArticle of activeArticles) {
             activeArticle.classList.remove('active');
         }
 
@@ -41,8 +41,8 @@
     }
 
     const optArticleSelector = '.post';
-    optTitleSelector = '.post-title';
-    optTitleListSelector = '.titles';
+        optTitleSelector = '.post-title';
+        optTitleListSelector = '.titles';
 
     function generateTitleLinks() {
         
@@ -50,7 +50,7 @@
 
         const titleList = document.querySelector(optTitleListSelector);
 
-        function clearTitleList(){
+        function clearTitleList() {
             titleList.innerHTML = '';
         }
 
@@ -62,42 +62,33 @@
         
         let html = '';
 
-        for(let article of articles){
+        for (let article of articles) {
 
             /* [DONE] get the article id */
 
             const articleId = article.getAttribute('id');
 
-            /* [DONE] find the title element */
-            /* [DONE]  get the title from the title element */
+            /* [DONE]  find the title element & get the title from the title element */
             
             const articleTitle = article.querySelector(optTitleSelector).innerHTML;
 
-            console.log(articleTitle);
-
             /* create HTML of the link */
 
-            const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
-
-            console.log(linkHTML);
+            const linkHTML = '<li> <a href="#' + articleId + '"> <span>' + articleTitle + '</span> </a> </li>';
 
             /* insert link into titleList */
 
             html = html + linkHTML;
-
         }
 
         titleList.innerHTML = html;
 
         const links = document.querySelectorAll('.titles a');
 
-        for(let link of links){
+        for (let link of links) {
             link.addEventListener('click', titleClickHandler);
         }
-
-        console.log (links);
     }
 
     generateTitleLinks();
-
 }
