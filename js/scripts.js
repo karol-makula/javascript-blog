3a14001605602592ad994b0111fcbe1c70648192
Karol Makuła <karol.makula@gmail.com>
2023-05-13 19:58:46 +0200

    Add rest scripts for titleClickHandler

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,41 @@
 'use strict';
 
 {
-    /*document.getElementById('test-button').addEventListener('click', function(){
-        const links = document.querySelectorAll('.post a');
-        console.log('links:', links);
-      });*/
-    
     const titleClickHandler = function(){
-        console.log(event);
+        event.preventDefault();
+        const clickedElement = this;
+        console.log('Link was clicked');
 
-        /* remove class 'active' from all article links  */
+        /* [DONE] remove class 'active' from all article links  */
         const activeLinks = document.querySelectorAll('.titles a.active')
             
         for(let activeLink of activeLinks){
                 activeLink.classList.remove('active');
         }
-        /* add class 'active' to the clicked link */
+        /* [DONE] add class 'active' to the clicked link */
+        clickedElement.classList.add('active');
 
-        /* remove class 'active' from all articles */
+        console.log('clickedElement:', clickedElement);
+
+        /* [DONE] remove class 'active' from all articles */
         const activeArticles = document.querySelectorAll('.post.active')
         
         for(let activeArticle of activeArticles){
             activeArticle.classList.remove('active');
         }
 
-        /* get 'href' attribute from the clicked link */
+        /* [DONE] get 'href' attribute from the clicked link */
+        const articleSelector = clickedElement.getAttribute('href');
+        
+        console.log('ID:', articleSelector);
 
         /* find the correct article using the selector (value of 'href' attribute) */
+        const targetArticle = document.querySelector(articleSelector);
 
-        /* add class 'active' to the correct article */
+        console.log('Atrykuł: ', targetArticle);
+
+        /* [DONE] add class 'active' to the correct article */
+        targetArticle.classList.add('active');
 
     }
 
